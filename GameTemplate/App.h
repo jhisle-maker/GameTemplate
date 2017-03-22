84e0d0f72a41f3acad95e363144eeabdb27e38fe
matti <matti@DESKTOP-EC8NAS6>
2017-03-22 14:05:48 +0100

    test

--- GameTemplate/App.h
+++ GameTemplate/App.h
@@ -6,6 +6,7 @@
 
 namespace GameTemplate
 {
+	//Test comment other
 	// Main entry point for our app. Connects the app with the Windows shell and handles application lifecycle events.
 	ref class App sealed : public Windows::ApplicationModel::Core::IFrameworkView
 	{
